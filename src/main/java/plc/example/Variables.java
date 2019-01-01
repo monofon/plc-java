38678a5d621e31a19cde04bb8a2df2f18c898878
monofon <monofon@tramberend.de>
2019-01-01 21:08:52 +0100

    Reconstruct lost file

---
+++ src/main/java/plc/example/Variables.java
@@ -0,0 +1,9 @@
+package plc.example;
+
+// start snippet classVars
+class Variables {
+    static final int a = 1;
+    static final int b = 2;
+    static final int c = a + b;
+}
+// end snippet classVars
